17f3cbaf7aa27dc764bff08a7705f06143a0f024
voorevamshi <vvamshijava@gmail.com>
2025-08-24 18:40:35 +0530

    initial kafka producer code

---
+++ src/main/java/com/vmc/KafkaProducerExampleApplication.java
@@ -0,0 +1,14 @@
+package com.vmc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerExampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vmc/controller/EventController.java
@@ -0,0 +1,42 @@
+package com.vmc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vmc.dto.Customer;
+import com.vmc.service.KafkaMessagePublisher;
+
+@RestController
+@RequestMapping("/producer-app")
+public class EventController {
+
+    @Autowired
+    private KafkaMessagePublisher publisher;
+
+    @GetMapping("/publish/{message}")
+    public ResponseEntity<?> publishMessage(@PathVariable String message) {
+        try {
+            for (int i = 0; i <= 1; i++) {
+                publisher.sendMessageToTopic(message + " : " + i);
+            }
+            return ResponseEntity.ok("message published successfully ..");
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
+    }
+
+    @PostMapping("/publish")
+    public void sendEvents(@RequestBody Customer customer) {
+        publisher.sendEventsToTopic(customer);
+    }
+
+
+}
